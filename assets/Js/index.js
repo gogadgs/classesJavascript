dde1a538ac1d155ade64560fcc859ab8c05b3784
douglas godois dos santos <doug_potter92@hotmail.com>
2025-01-16 22:22:42 -0300

    primeiro commit

---
+++ assets/Js/index.js
@@ -0,0 +1,57 @@
+class  heroi {
+    constructor(nome,idade,tipo){
+            this.nome = nome;
+            this.idade = idade;
+            this.tipo = tipo;
+
+    }
+
+    /*  metodos getter and setter */
+
+    getNome(){
+        return this.nome;
+
+    };
+    setNome(nome){
+        return this.nome = nome;
+    };
+
+    getIdade(){
+        return this.idade;
+    };
+    setIdade(idade){
+        return this.idade = idade;
+    };
+
+    getTipo(){
+        return this.tipo
+    };
+
+    setTipo(tipo){
+        return this.tipo = tipo;
+    };
+
+   
+
+    atacar(){
+        let ataquesHerois = ["espada","magia","artes marciais","shuriken","ataque cibernetico"];
+        if(this.tipo ==="guerreiro"){
+            console.log(`O ${this.tipo} atacou usando ${ataquesHerois[0]}`)
+        }else if(this.tipo === "mago"){
+            console.log(`O ${this.tipo} atacou usando ${ataquesHerois[1]}`)
+        }else if(this.tipo === "monge"){
+            console.log(`O ${this.tipo} atacou usando ${ataquesHerois[2]}`)
+        }else if(this.tipo === "ninja"){
+            console.log(`O ${this.tipo} atacou usando ${ataquesHerois[3]}`)
+        }else if(this.tipo === "robo"){
+            console.log(`O ${this.tipo} atacou usando ${ataquesHerois[4]}`)
+        }
+
+        
+    }
+}
+
+
+const meuHeroi = new heroi("homem de ferro",37,"guerreiro");
+
+console.log(meuHeroi.atacar());
